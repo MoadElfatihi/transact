51331788341c130c7bfb49712ccf8c20716213a5
Moad <elfatihi.moad@gmail.com>
2025-07-10 16:01:07 +0200

    project set up

---
+++ src/main/java/com/transacthex/transacthex/TransactHexApplication.java
@@ -0,0 +1,13 @@
+package com.transacthex.transacthex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactHexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactHexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/transacthex/transacthex/TransactHexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transacthex.transacthex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactHexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

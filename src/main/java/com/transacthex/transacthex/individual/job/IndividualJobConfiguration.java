7b398f7167e509aaf3cfdb9cf4dc1e807528b06b
Moad <elfatihi.moad@gmail.com>
2025-07-11 12:44:22 +0200

    add logging from individual step listener

--- src/main/java/com/transacthex/transacthex/individual/job/IndividualJobConfiguration.java
+++ src/main/java/com/transacthex/transacthex/individual/job/IndividualJobConfiguration.java
@@ -1,9 +1,11 @@
 package com.transacthex.transacthex.individual.job;
 
+import com.transacthex.transacthex.individual.job.listener.IndividualStepListener;
 import com.transacthex.transacthex.individual.job.mapper.IndividualFieldSetMapper;
 import com.transacthex.transacthex.individual.job.model.IndividualDto;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -14,6 +16,7 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -40,15 +43,19 @@ public class IndividualJobConfiguration {
                 .<IndividualDto,IndividualDto> chunk(10, transactionManager)
                 .reader(individualReader)
                 .writer(individualWriter)
+                .allowStartIfComplete(true)
+                .listener(new IndividualStepListener())
                 .build();
 
     }
 
+    //@Value("#{jobParameters['filePath']}")String filePath
     @Bean("individualReader")
     @StepScope
     public  FlatFileItemReader<IndividualDto> getIndividualReader() {
         var itemReader = new FlatFileItemReader<IndividualDto>();
         itemReader.setLinesToSkip(1);
+        //"files/individuals.txt"
         itemReader.setResource(new FileSystemResource("files/individuals.txt"));
         var lineMapper = new DefaultLineMapper<IndividualDto>();
         lineMapper.setLineTokenizer(new DelimitedLineTokenizer());
